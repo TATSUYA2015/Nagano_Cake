7557f7d206f4a34723afa9941f1f68d3b68a23c0
EC2 Default User <112598954+TATSUYA2015@users.noreply.github.com>
2022-11-04 14:04:36 +0000

    add-commit

---
+++ app/controllers/admin/customers_controller.rb
@@ -0,0 +1,12 @@
+class Admin::CustomersController < ApplicationController
+  def index
+    @customer=Customer.new
+    @customers=Customer.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+
   namespace :admin do
     resources :items, only:[:new, :index, :show, :edit, :create, :update]
+    resources :customers, only:[:index, :show, :edit, :update]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
