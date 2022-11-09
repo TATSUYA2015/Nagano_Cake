29fc9a8685cfeaa23c3b50347e676c2e98d0e94b
EC2 Default User <112598954+TATSUYA2015@users.noreply.github.com>
2022-11-09 16:12:20 +0000

    add-commit

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,6 +4,7 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer=Customer.find(params[:id])
   end
 
   def update
@@ -13,6 +14,12 @@ class Public::CustomersController < ApplicationController
   end
 
   def withdraw
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
   end
 
 end
