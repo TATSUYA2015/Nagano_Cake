18cd4120810537c147e127fb1bfbb44bec6934f8
EC2 Default User <112598954+TATSUYA2015@users.noreply.github.com>
2022-11-16 16:42:02 +0000

    add_commit

--- config/routes.rb
+++ config/routes.rb
@@ -13,11 +13,12 @@ Rails.application.routes.draw do
 
 
   namespace :admin do
+    get 'homes/top'
     resources :items, only:[:new, :index, :show, :edit, :create, :update]
     resources :customers, only:[:index, :show, :edit, :update]
     resources :genres, only:[:index, :create, :edit, :update]
     resources :addresses, only:[:index, :edit, :create, :update, :destroy]
-    resources :orders, only:[:index, :show, :update]
+    resources :orders, only:[:show, :update]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
