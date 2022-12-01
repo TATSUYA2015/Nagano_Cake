a85267187515e8fa029b5b9d88c057367458c418
EC2 Default User <112598954+TATSUYA2015@users.noreply.github.com>
2022-12-01 13:29:26 +0000

    add-commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,24 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+
   def after_sign_in_path_for(resource)
-    items_path
+    case resource
+    when Admin
+      admin_homes_top_path
+    when Customer
+      items_path
+    end
   end
 
-  def after_sign_out_path_for(resource)
-    root_path
-  end
+   def after_sign_out_path_for(resource)
+    if resource == :admin
+      new_admin_session_path
+    else
+      root_path
+    end
+   end
 
   protected
 
